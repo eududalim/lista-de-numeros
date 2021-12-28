0df6c5978deb8baac03751c386d48d2e8a102921
Eduarda Lima <eududalim@gmail.com>
2021-12-28 12:52:37 -0300

    Implementando funçao gerar lista

--- numbers_list/lib/api/api.dart
+++ numbers_list/lib/api/api.dart
@@ -2,14 +2,13 @@ import 'package:url_launcher/url_launcher.dart';
 
 class WhatsAppApi {
   ///Implementar esta função corretamente!
-  abrirWhatsApp() async {
-    var whatsappUrl =
-        "whatsapp://send?phone=+5586994324465&text=Olá,tudo bem ?";
+  static abrirWhatsApp(String phone) async {
+    var whatsappUrl = "whatsapp://send?phone=+55$phone";
 
     if (await canLaunch(whatsappUrl)) {
       await launch(whatsappUrl);
     } else {
-      throw 'Could not launch $whatsappUrl';
+      throw 'Aplicativo não encontrado';
     }
   }
 }
